bf71394fadb17d1205fc90c8a3d09602da4321aa
Todd Eidson <todd@eidson.info>
2019-07-26 07:38:35 -0400

    feat: init commit

---
+++ ToddNorris/Models/JokeObject.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace ToddNorris.Models
+{
+    public class JokeObject
+    {
+        [JsonProperty("value")] public string Value { get; set; }
+    }
+}
---
+++ ToddNorris/Models/ToddNorris.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace ToddNorris.Models
+{
+    public class ToddNorris
+    {
+        private static readonly HttpClient Client = new HttpClient();
+
+        public string Get()
+        {
+            Task<string> requestTask = RequestTask();
+            requestTask.Wait();
+            var jokeObject = JsonConvert.DeserializeObject<JokeObject>(requestTask.Result);
+            
+            return jokeObject.Value.Replace("Chuck", "Todd").Replace("chuck", "Todd").Replace("Norris'", "Eidson's").Replace("Norris", "Eidson");
+        }
+
+        private async Task<string> RequestTask()
+        {
+            
+             return await Client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+        }
+        
+    }
+}
