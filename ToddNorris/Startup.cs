b08ed9ac9e046f8bcabdee0525c54ec29b86e87d
Todd Eidson <todd@eidson.info>
2019-07-31 17:56:14 -0400

    build(dotnet): updates to 2.2

--- ToddNorris/Startup.cs
+++ ToddNorris/Startup.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.CookiePolicy;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SQLitePCL;
+
 
 namespace ToddNorris
 {
